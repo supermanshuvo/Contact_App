fc58e644e4982268db8b35f0208b763cdeabca24
supermanshuvo <shuvothesuperman@gmail.com>
2021-01-16 12:10:47 +0600

    Add form-group

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -4,18 +4,21 @@
     <div class="row justify-content-center">
         <form class="form-signin">
             <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-            <label for="inputEmail" class="sr-only">Email address</label>
-            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            <div class="form-group">
+                <label for="inputEmail" class="sr-only">Email address</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+            </div>
+            <div class="form-group">
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            </div>
             <div class="checkbox mb-3">
                 <label>
-                <input type="checkbox" value="remember-me"> Remember me
+                    <input type="checkbox" value="remember-me"> Remember me
                 </label>
             </div>
             <button class="btn btn-primary" type="submit">Sign in</button>
             <button class="btn btn-info">Create Account</button>
-            <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
         </form>
     </div>
 </div>
