b59c4180176078c43d27782fa5425e5b20b54046
supermanshuvo <shuvothesuperman@gmail.com>
2021-01-16 11:37:27 +0600

    Fix the position of the js file position and beautify the html file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name')}}</title>
     <link rel="stylesheet" href="{{ Asset('css/app.css')}}">
 </head>
+
 <body>
-<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-      <h5 class="my-0 mr-md-auto font-weight-normal">Contact App</h5>
-      <nav class="my-2 my-md-0 mr-md-3">
-        <a class="p-2 text-dark" href="#">Home</a>
-        <a class="p-2 text-dark" href="#">Name</a>
-      </nav>
-      <a class="btn btn-outline-primary" href="#">Sign up</a>
-      @yield('content')
-      <script src="{{ Asset('js/app.js')}}"></script>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+        <h5 class="my-0 mr-md-auto font-weight-normal">Contact App</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Home</a>
+            <a class="p-2 text-dark" href="#">Name</a>
+        </nav>
+        <a class="btn btn-outline-primary" href="#">Sign up</a>
     </div>
+    @yield('content')
+    <script src="{{ Asset('js/app.js')}}"></script>
 </body>
+
 </html>
