610b1f57f7a4d8d1b99d76a8b49a5955c08360a6
supermanshuvo <shuvothesuperman@gmail.com>
2021-01-16 12:23:29 +0600

    Create New register file and login file

---
+++ resources/views/contact/register.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <form>
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <div class="form-group">
+                <label for="inputEmail" class="sr-only">Email address</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+            </div>
+            <div class="form-group">
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary" type="submit">Sign in</button>
+                <button class="btn btn-info">Create Account</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
