6e939071ab6d049f6466643a4b6f3ac98c0c4fbf
supermanshuvo <shuvothesuperman@gmail.com>
2021-01-17 23:31:14 +0600

    Add margin top in the index file row

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container">
     <h1 class="text-center">Welcome to the Contact app</h1>
-    <div class="row">
+    <div class="row mt-5">
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
             <h3>User name</h3>
             <a href="{{ url('/logout') }}"><input type="button" value="Logout" class="btn btn-warning"></a>
