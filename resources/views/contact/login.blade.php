e4083ba94af0baa0fd5f18901fd9bf294bc472ea
supermanshuvo <shuvothesuperman@gmail.com>
2021-01-22 00:29:11 +0600

    Change css and add user svg

--- resources/views/contact/login.blade.php
+++ resources/views/contact/login.blade.php
@@ -4,37 +4,37 @@
     <div class="row d-flex justify-content-center">
         <div class="card bg-style">
             <div class="card-body">
-            <div class="row">
-                <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 col-xl-6">
-                    <div class="text-center">
-                        <h3>Welcome</h3>
-                        <p>to</p>
-                        <h3>Contact App</h3>
+                <div class="row">
+                    <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 col-xl-6">
+                        <div class="text-center">
+                            <h3>Welcome</h3>
+                            <p>to</p>
+                            <h3>Contact App</h3>
+                            <img src="img/user-circle-regular.svg" alt="There is an User Image">
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                            <form>
+                                <h1 class="h3 mb-3 font-weight-normal">Login Now</h1>
+                                <div class="form-group">
+                                    <label for="inputEmail">Email address</label>
+                                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address"
+                                        required autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputPassword">Password</label>
+                                    <input type="password" id="inputPassword" class="form-control"
+                                        placeholder="Password" required>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" value="Login" name="submit" class="btn btn-primary">
+                                </div>
+                                <p>Don't have an account?<a href="{{ url('/register') }}"> Sign Up</a></p>
+                            </form>
+                        </div>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                    <form>
-                        <h1 class="h3 mb-3 font-weight-normal">Login Now</h1>
-                        <div class="form-group">
-                            <label for="inputEmail">Email address</label>
-                            <input type="email" id="inputEmail" class="form-control" placeholder="Email address"
-                                required autofocus>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputPassword">Password</label>
-                            <input type="password" id="inputPassword" class="form-control" placeholder="Password"
-                                required>
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" value="Login" name="submit" class="btn btn-primary">
-                        </div>
-                        <p>Don't have an account?<a href="{{ url('/register') }}"> Sign Up</a></p>
-                    </form>
-                </div>
-            </div>
             </div>
         </div>
     </div>
-</div>
 
-@endsection
+    @endsection
