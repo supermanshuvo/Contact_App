a46b066e8f49c52315ce3a01f7adc0f7c7a83d1b
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-01 08:40:09 +0600

    Add contact_ in the app blade file in the menu and route name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,17 +17,17 @@
 <body>
     <!-- START NAVBAR -->
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="{{ route('index') }}">{{ config('app.name')}}</a>
+      <a class="navbar-brand" href="{{ route('contact_index') }}">{{ config('app.name')}}</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="{{ route('index') }}">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="{{ route('contact_index') }}">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="{{  route('add') }}">Add New Contact<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="{{  route('contact_add') }}">Add New Contact<span class="sr-only">(current)</span></a>
           </li>
         </ul>
         <form class="form-inline mt-2 mt-md-0">
