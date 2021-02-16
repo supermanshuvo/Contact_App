c4b33c3bbccf19509438aa066bc282d2fe5b98ac
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-16 21:59:09 +0600

    Add contact model in login controller

--- app/Http/Controllers/BackEnd/LoginController.php
+++ app/Http/Controllers/BackEnd/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\BackEnd;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Contact;
 
 class LoginController extends Controller
 {
