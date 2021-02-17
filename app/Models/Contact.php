97752a87f3a067a3207f97d58abace0f24ab891d
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-17 22:27:41 +0600

    Add Table function in Model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
+
+
+    public function number(){
+        return $this->hasOne('App\Models\Number');
+    }
 }
--- app/Models/Number.php
+++ app/Models/Number.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Number extends Model
 {
     use HasFactory;
+    protected $table = "numbers";
+
+    public function contact(){
+        return $this->belongsTo('App\Models\Contact');
+    }
 }
