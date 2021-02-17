84503efbdef3a9457d623729da5d2240cb84c473
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-17 23:11:01 +0600

    Add protect in  in Contact Model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
-
+    protected $table = 'contacts';
     protected $fillable = [
         'name',
         'email',
