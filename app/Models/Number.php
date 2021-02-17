6eb4c639d9de326c30bf3e6f0361d922ab64fb61
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-17 22:47:47 +0600

    Add data protceted and fillable in number

--- app/Models/Number.php
+++ app/Models/Number.php
@@ -9,7 +9,9 @@ class Number extends Model
 {
     use HasFactory;
     protected $table = "numbers";
-
+    protected $fillable = [
+        'name','number'
+    ];
     public function contact(){
         return $this->belongsTo('App\Models\Contact');
     }
