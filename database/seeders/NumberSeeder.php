b43799bd2cb229b5a340055bd475fef60900f520
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-18 23:31:16 +0600

    remove some not used use line

--- database/seeders/NumberSeeder.php
+++ database/seeders/NumberSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use App\Models\Number;
 
 class NumberSeeder extends Seeder
