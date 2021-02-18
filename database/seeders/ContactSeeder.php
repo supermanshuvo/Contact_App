c48d5051a2228180bd38d6b4e7c7aab1547b30e0
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-19 02:16:08 +0600

    Make commented seeder data

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -14,23 +14,40 @@ class ContactSeeder extends Seeder
      */
     public function run()
     {
-        Contact::create([
-            'name'=> 'Shuvo',
-            'email'=>'shuvo@gmail.com',
-            'number' => '01791623736',
-            'password' => '654321'
-        ]);
-        Contact::create([
-            'name'=> 'Test',
-            'email'=>'test@gmail.com',
-            'number' => '01303316865',
-            'password' => '987654'
-        ]);
-        Contact::create([
-            'name'=> 'Test2',
-            'email'=>'test2@gmail.com',
-            'number' => '01985264589',
-            'password' => '123456'
-        ]);
+
+        // factory(Contact::class, 50)->create()->each(function($u) {
+        //     $u->number()->save(factory(Number::class)->make());
+        // });
+        $contact = new Contact([
+                'name'=> 'Shuvo',
+                'email'=>'shuvo@gmail.com',
+                'number' => '01791623736',
+                'password' => '654321'
+            ]);
+            $contact->save();
+        $number  = new Number([
+                'name' => 'Shuvo',
+                'number' => '01791623736',
+                ]);
+            $contact->number()->save($number);
+
+        // Contact::create([
+        //     'name'=> 'Shuvo',
+        //     'email'=>'shuvo@gmail.com',
+        //     'number' => '01791623736',
+        //     'password' => '654321'
+        // ]);
+        // Contact::create([
+        //     'name'=> 'Test',
+        //     'email'=>'test@gmail.com',
+        //     'number' => '01303316865',
+        //     'password' => '987654'
+        // ]);
+        // Contact::create([
+        //     'name'=> 'Test2',
+        //     'email'=>'test2@gmail.com',
+        //     'number' => '01985264589',
+        //     'password' => '123456'
+        // ]);
     }
 }
--- database/seeders/NumberSeeder.php
+++ database/seeders/NumberSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\Number;
+use App\Models\Contact;
 
 class NumberSeeder extends Seeder
 {
@@ -14,21 +16,34 @@ class NumberSeeder extends Seeder
      */
     public function run()
     {
-        Number::create(
-            [
-            'name' => 'Shuvo',
-            'number' => '01791623736',
-            'userId' => 1
-            ]);
-        Number::create([
-            'name' => 'Superman',
-            'number' => '01303316865',
-            'userId' => 2
-        ]);
-        Number::create([
-            'name' => 'Rezaul Karim',
-            'number' => '01713467045',
-            'userId' => 1
-        ]);
+
+        // DB::table('numbers')->insert([
+        //     'name' => 'Shuvo',
+        //     'number' => '01791623736',
+        //     'userId' => 1
+        // ]);
+
+        // $number  = new Number([
+        //     'name' => 'Shuvo',
+        //     'number' => '01791623736',
+        //     ]);
+        // $contact = Contact::find(1);
+        // $contact->number()->save($number);
+        // Number::create(
+        //     [
+        //     'name' => 'Shuvo',
+        //     'number' => '01791623736',
+        //     'userId' => 1
+        //     ]);
+        // Number::create([
+        //     'name' => 'Superman',
+        //     'number' => '01303316865',
+        //     'userId' => 2
+        // ]);
+        // Number::create([
+        //     'name' => 'Rezaul Karim',
+        //     'number' => '01713467045',
+        //     'userId' => 1
+        // ]);
     }
 }
