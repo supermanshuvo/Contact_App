c13b669f33859df93ec6a488622da45a37e31791
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-19 16:45:23 +0600

    Add Fail alert in login and user custom auth in loginController

--- app/Http/Controllers/BackEnd/LoginController.php
+++ app/Http/Controllers/BackEnd/LoginController.php
@@ -25,5 +25,15 @@ class LoginController extends Controller
             'email'=> 'required|email',
             'password'=> 'required|min:6|max:16',
         ]);
+        $user = Contact::where('email','=', $request->email)->first();
+        if($user){
+            if(Hash::check($request->password, $user->password)){
+                
+            }else{
+                return redirect('login')->with('fail','Invalid Password try again!');    
+            }
+        }else{
+            return redirect('login')->with('fail','No Account found in this email try again!');
+        }
     }
 }
