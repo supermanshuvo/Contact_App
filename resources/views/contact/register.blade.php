4f96ba97371909a4254e280d6f9df857507e9076
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-19 09:56:48 +0600

    Customize the alert function in register file

--- resources/views/contact/register.blade.php
+++ resources/views/contact/register.blade.php
@@ -10,13 +10,14 @@
             <div class="card-body">
                 <!-- Alert Area -->
                 @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
+                @foreach ($errors->all() as $error)       
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                     {{ $error }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
+                @endforeach
                 @endif
                 <!-- End Alert Area -->
                 <!-- START REGISTER FORM -->
