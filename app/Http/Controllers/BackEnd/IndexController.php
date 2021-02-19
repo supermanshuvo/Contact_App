160b79ce3234bbba7fb8d2b55d889ca54b438f06
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-19 17:06:05 +0600

    Create a user data in indexController

--- app/Http/Controllers/BackEnd/IndexController.php
+++ app/Http/Controllers/BackEnd/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\BackEnd;
 
 use App\Http\Controllers\Controller;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use App\Models\Number;
 
@@ -10,12 +11,16 @@ class IndexController extends Controller
 {
     public function index()
     {
+        if(session()->has('user')){
+            $user = Contact::where('id','=', session('user'))->first();
+            $data = [
+                'user'=> $user
+            ];
+        }
         $data =Number::all();
         return view('contact.index',['numbers'=>$data]);
     }
 
-    
-    
     public function delete($id)
     {
         $delete = Number::find($id);
