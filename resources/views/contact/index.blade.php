04fd7e892d97266b2d907d18d718f4083c0ac8db
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-20 01:30:24 +0600

    Change the alert

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -22,13 +22,19 @@
         <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 ml-5">
             <div class="resutls">
                 @if(Session::get('success'))
-                <div class="alert alert-success">
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
                     {{ Session::get('success') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 @endif
                 @if(Session::get('delete'))
-                <div class="alert alert-warning">
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
                     {{ Session::get('delete') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 @endif
             </div>
@@ -49,7 +55,8 @@
                         <td>{{ $number['NumberNumber'] }}</td>
                         <td><a href="edit/{{$number->id}}"><i class="fa fa-edit"
                                     style="font-size:24px;color:blue"></i></a> <a href="delete/{{$number->id}}"><i
-                                    class="fa fa-trash-o" onclick="confirm('Are you sure! You want to delete number!');" style="font-size:24px;color:red"></i></a></td>
+                                    class="fa fa-trash-o" onclick="confirm('Are you sure! You want to delete number!');"
+                                    style="font-size:24px;color:red"></i></a></td>
                     </tr>
                     @endforeach
                 </tbody>
