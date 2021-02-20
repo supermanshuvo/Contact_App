391a7835791c38536f34c0f2225544543da25bfa
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-20 11:53:00 +0600

    Add new alert in add blade for fail or duplicate data

--- resources/views/contact/add.blade.php
+++ resources/views/contact/add.blade.php
@@ -24,6 +24,14 @@
                             </button>
                         </div>
                         @endif
+                        @if(Session::get('fail'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            {{ Session::get('fail') }}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        @endif
                     </div>
                     <!-- End Alert Area -->
                     <div class="form-group">
