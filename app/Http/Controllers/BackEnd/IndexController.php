31b554eea81edf215c1f55c1f5f30e5990530156
supermanshuvo <shuvothesuperman@gmail.com>
2021-02-22 01:09:13 +0600

    Testing new search way

--- app/Http/Controllers/BackEnd/IndexController.php
+++ app/Http/Controllers/BackEnd/IndexController.php
@@ -40,8 +40,60 @@ class IndexController extends Controller
             $validateData = $request->validate([
                 'search' => 'required',
             ]);
-            $data = $request->search;
-            $info = DB::table('numbers')->where('userId','=',session('UserId'))->where('NumberNumber','LIKE','%',$data,'%')->orWhere('NumberName','LIKE','%',$data,'%')->get();
+
+            $data = '%'.$request->search.'%';
+            // $data = $request->input('search');
+/* 
+            $info = DB::table('numbers')->where('NumberNumber','LIKE',$data)->orWhere('NumberName','LIKE',$data)
+                            ->where(function ($query) {
+                                 $query->where('userId','=',session('UserId'));
+                            })
+                            ->get(); */
+                            $info =DB::table('numbers')->where('userId','=',session('UserId'))->where('NumberNumber','LIKE',$data)->orWhere('NumberName','LIKE',$data)->get();
+/* 
+            $info = DB::table('numbers')->where('userId','=',session('UserId'))
+                            ->when($data, function ($query, $data) {
+                                return $query->where('NumberNumber','LIKE',$data)->orWhere('NumberName','LIKE',$data);
+                            })
+                            ->get(); */
+            return $info;
+
+            /* $role = $request->input('role');
+
+            $ifo = DB::table('users')
+                ->when($role, function ($query, $role) {
+                    return $query->where('role_id', $role);
+                })
+                ->get(); */
+
+
+            
+            /* $sql = "SELECT * FROM contact.numbers WHERE userId = 3 AND (numbers.NumberName LIKE '".$data."' OR numbers.NumberNumber LIKE '".$data."');";
+            $info = DB::raw($sql);
+            return $info; */
+            // dd($sql);
+            // $info = DB::raw($sql);
+            /* $info = DB::table('numbers')
+           ->where('userId', '=', session('UserId'))
+           ->where(function ($query) {
+               $query->where('NumberNumber',"LIKE",$GLOBALS['data'])
+                     ->orWhere('NumberName',"LIKE",$GLOBALS['data']);
+           })
+           ->get(); */
+
+
+            /* $info = DB::table('numbers')->where('userId','=',session('UserId'))->where('NumberNumber','LIKE',$data)->orWhere('NumberName',"LIKE",$data)->get();
+ */
+                /* $test = DB::table('numbers')->where('userId',session('UserId'))->where("NumberName","LIKE '",$data,"'")->orWhere("NumberNumber","LIKE '",$data,"'");
+ */
+            // $info = DB::table('numbers')->where('userId',session('UserId'))->orwhere('NumberNumber','LIKE','%',$data,'%')->orWhere('NumberName','%',$data,'%')->get();
+
+
+
+
+
+                /* print_r($test);
+                die(); */
             return $info;
             
             
